78748d3aafb5836b466fa9c5a35ae3934a41f812
ribeiro.filipe94@gmail.com <ribeiro.filipe94@gmail.com>
2020-02-06 18:40:48 -0300

    feat: finalizado modulo de testes no reactJS

--- src/__tests__/TechList.test.js
+++ src/__tests__/TechList.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
+import { useDispatch } from 'react-redux';
 
+import { addTech } from '~/store/modules/techs/actions';
 import TechList from '~/components/TechList';
 
 jest.mock('react-redux');
@@ -17,5 +19,18 @@ describe('Techslist component', () => {
     
     expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
     expect(getByTestId('tech-list')).toContainElement(getByText('ReactJS'));
+  });
+
+  it('should be able to add new tech', () => {
+    const { getByLabelText, getByTestId } = render(<TechList />);
+
+    const dispatch = jest.fn();
+
+    useDispatch.mockReturnValue(dispatch);
+
+    fireEvent.change(getByLabelText('Tech'), { target: { value: 'Node.js' }});
+    fireEvent.submit(getByTestId('tech-form'));
+
+    expect(dispatch).toBeCalledWith(addTech('Node.js'));
   });
 });
